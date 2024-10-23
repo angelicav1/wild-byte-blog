d1ebf3c2b4f14b5e13f7ac2737b587422cfabf88
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-10-23 16:35:31 -0700

    updated header.jsx

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,12 +2,16 @@ import "./header.css";
 
 export default function Header() {
     return (
-        <div className="header">
-            <div className="headerTitles">
-                <span className="headerTitleSmall">Wild Byte</span>
-                <span className="headerTitleLarge">Blog</span>
-            </div>
-            <img className="headerImage" src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/138/249/original/Yellowstone_mountains.jpg?1723263171" alt="Yellowstone Forest-Wild Byte Blog" />
+      <div className='header'>
+        <div className='headerTitles'>
+          <span className='headerTitleSmall'>Wild Byte</span>
+          <span className='headerTitleLarge'>Blog</span>
         </div>
-    )
+        <img
+          className='headerImage'
+          src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/138/249/original/Yellowstone_mountains.jpg?1723263171'
+          alt='Yellowstone Forest-Wild Byte Blog'
+        />
+      </div>
+    );
 }
