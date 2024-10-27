626157c31873f109924ac97ab6288eb29d4627ac
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-10-26 18:31:38 -0700

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import TopBar from "./components/topbar/TopBar";
 import Register from "./pages/register/Register";
+import { BrowserRouter, Route} from 'react-router-dom';
 
 
 
-function App() {
+export default function App() {
   return (
-    <>
-      <TopBar />
-      <Register/>
-      
-    </>
+    <BrowserRouter>
+      <Topbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/write' element={<Write />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
