ea02c6a3986afab6e2d4831bd9064764c0e24c10
Angelica Victoria <114718142+angelicav1@users.noreply.github.com>
2024-10-27 16:22:56 -0700

    updated index.html and App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import TopBar from "./components/topbar/TopBar";
+import Write from "./pages/write/Write";
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
 import { BrowserRouter, Route} from 'react-router-dom';
 
 
 
 export default function App() {
+  const user = true;
   return (
     <BrowserRouter>
       <Topbar />
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/write' element={<Write />} />
+        <Route path='/' exact element={<Home></Home>}></Route>
+        <Route
+          path='/register'
+          element={user ? <Home /> : <Register />}
+        ></Route>
+        <Route path='/login' element={user ? <Home /> : <Login />}></Route>
+        <Route path='/write' element={user ? <Write /> : <Register />}></Route>
+        <Route
+          path='/settings'
+          element={user ? <Settings /> : <Register />}
+        ></Route>
+        <Route path='/post/:postId'>
+          {' '}
+          <Single />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Route } from "react-router-dom";
 import "./topbar.css";
 
 export default function TopBar() {
+  const user = false;
     return (
       <div className='top'>
         <div className='topLeft'>
@@ -11,18 +13,51 @@ export default function TopBar() {
         </div>
         <div className='topCenter'>
           <ul className='topList'>
-            <li className='topListItem'>HOME</li>
-            <li className='topListItem'>ABOUT</li>
-            <li className='topListItem'>CONTACT</li>
-            <li className='topListItem'>WRITE</li>
-            <li className='topListItem'>LOGOUT</li>
+            <li className='topListItem'>
+              <Route className='route' to='/'>
+                HOME
+              </Route>
+            </li>
+            <li className='topListItem'>
+              <Route className='route' to='/'>
+                ABOUT
+              </Route>
+            </li>
+            <li className='topListItem'>
+              <Route className='route' to='/'>
+                CONTACT
+              </Route>
+            </li>
+            <li className='topListItem'>
+              <Route className='route' to='/'>
+                WRITE
+              </Route>
+            </li>
+            <li className='topListItem'>
+              {user && "LOGOUT"}
+              <Route className='route' to='/'>
+                LOGOUT
+              </Route>
+            </li>
           </ul>
         </div>
         <div className='topRight'>
-            <img className="topImage"
-              src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/138/232/original/Wordpress_Transparent.png?1723244265'
-              alt=''
-            />
+          {
+            user ? (
+
+              <img
+            className='topImage'
+            src='https://s3.amazonaws.com/shecodesio-production/uploads/files/000/138/232/original/Wordpress_Transparent.png?1723244265'
+            alt=''
+          />
+            ) : (
+              <ul className="topList">
+              <Route className="route" to="/login">LOGIN</Route>
+              <Route className="route" to="/register">REGISTER</Route>
+              </ul>
+            )
+          }
+          
           <i className='topSearchIcon fa-solid fa-magnifying-glass'></i>
         </div>
       </div>
